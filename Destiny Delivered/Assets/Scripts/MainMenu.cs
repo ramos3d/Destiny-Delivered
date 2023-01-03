2cef77cef6e3eb7720f460a3743bd21b55c5e261
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-01-03 19:33:45 -0400

    Start button activated. Load Game scene

--- Destiny Delivered/Assets/Scripts/MainMenu.cs
+++ Destiny Delivered/Assets/Scripts/MainMenu.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartGame(){
         SceneManager.LoadScene("Game");                 // Load the Scene Game.
+       // Application.LoadLevel("Game");                // This is obsolete
     }
 
 
     public void QuitGame(){
         Application.Quit();
-
     }
 }
