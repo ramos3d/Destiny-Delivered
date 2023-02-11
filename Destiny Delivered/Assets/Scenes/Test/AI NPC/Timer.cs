98a830d1196b4ce798f714108c84842d9278b378
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-02-10 23:01:17 -0400

    Timer test implemented

--- Destiny Delivered/Assets/Scenes/Test/AI NPC/Timer.cs
+++ Destiny Delivered/Assets/Scenes/Test/AI NPC/Timer.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text timerText;
+    public TMP_Text milliText;
+    public float timeValue = 30f;
+    public int milliseconds;
 
-    // Update is called once per frame
+private void Start() {
+    
+}
     void Update()
     {
-        
+        if(timeValue > 0){
+            timeValue -= Time.deltaTime;   
+
+        }else
+        {
+            timeValue = 0;
+            milliseconds = 0;   
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay){
+        if(timeToDisplay < 0){
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay/60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+         milliseconds = Mathf.FloorToInt(Time.deltaTime * 1000);
+
+        timerText.text = string.Format("{0:00}:{0:30}", minutes, seconds);
+        milliText.text = string.Format("{0:00}", milliseconds);
     }
 }
