dbbfc3aaf6e9d66b7192093cb80c0ea5222270d9
themadrat <Jaredshaddick@gmail.com>
2023-03-10 14:58:48 -0400

    Revert "UI Refactored code"
    This reverts commit 48d574fc3cba0b19a1ef48602adc54862f609b50.

--- Destiny Delivered/Assets/Scenes/Test/AI NPC/CarSelfDrive.cs
+++ Destiny Delivered/Assets/Scenes/Test/AI NPC/CarSelfDrive.cs
@@ -53,8 +53,10 @@ public class CarSelfDrive : MonoBehaviour
             {
                 nodes.Add(pathTransforms[i]);
                 
-            }   
+            }
+            
         }
+
     }
 
 
--- Destiny Delivered/Assets/Scripts/PlayerCar.cs
+++ Destiny Delivered/Assets/Scripts/PlayerCar.cs
@@ -13,7 +13,7 @@ public class PlayerCar : Cars
     [SerializeField] CinemachineVirtualCamera lookLeftCam;
     
     [SerializeField] public HealthBar health_bar;
-    private readonly float damage = 0.1f ;
+    private readonly float damage = 1 ;
     public float new_axi = -0.9f;
 
     // Gamepad gamepad = Gamepad.current;
@@ -42,18 +42,10 @@ public class PlayerCar : Cars
         {
             Move(); 
         }
-        
-        if(Input.GetKeyDown(KeyCode.W)){
-            Move(); 
-        }
-        if(Input.GetKeyDown(KeyCode.E)){
-            Break();
-        }
-        
+        Break();
     }
 
     private void Update(){
-     
         if(Timer._go == true &&  GameController.game_state == true)
         {
             AnimateWheels();
@@ -85,6 +77,11 @@ public class PlayerCar : Cars
                     //Debug.Log("Look to Back");
                 }
             }
+                
+         
+           
+           
+            
         }
        
         if (health_bar.GetCurrentHealth() <= 0)
@@ -93,15 +90,10 @@ public class PlayerCar : Cars
         }
     }
 
-
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag != "Player" && other.gameObject.tag != "BreakLight")
         {
-            if (!Timer._delivery_completed)
-            {
-                PlayerDamage(); 
-                
-            }
+            PlayerDamage(); 
         }
     }
         
@@ -111,23 +103,17 @@ public class PlayerCar : Cars
     }
 
     private void LateUpdate() {
-        if (!GameManager.usingKeyBoard)
+        float turnAxis = gamepad.leftStick.x.ReadValue(); // Lê o eixo X do stick esquerdo
+         // Rotação das rodas
+        float newTurnAxis = gamepad.leftStick.x.ReadValue(); // Lê o eixo X do stick esquerdo
+        if (Mathf.Abs(newTurnAxis) > 0.1f) // Verifica se o stick esquerdo está sendo movido para a direita ou esquerda
         {
-            float turnAxis = gamepad.leftStick.x.ReadValue();               // Lê o eixo X do stick esquerdo
-            // Rotação das rodas
-            float newTurnAxis = gamepad.leftStick.x.ReadValue();            // Lê o eixo X do stick esquerdo
-            
-        if (Mathf.Abs(newTurnAxis) > 0.1f)                              // Verifica se o stick esquerdo está sendo movido para a direita ou esquerda
-        {
-            turnAxis = newTurnAxis;                                     // Atualiza o eixo de rotação das rodas
+            turnAxis = newTurnAxis; // Atualiza o eixo de rotação das rodas
             Turn();
         }
         else
         {
-            turnAxis = 0f;                                              // Reseta o eixo de rotação das rodas
-            Turn();
-        }
-        }else{
+            turnAxis = 0f; // Reseta o eixo de rotação das rodas
             Turn();
         }
 
